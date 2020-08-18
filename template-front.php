b33b60a8cf6cd29a660da5477c0721eda9260c54
Marc Perrien <marc@chaosiq.io>
2020-08-19 01:54:41 +0200

    Add adults and elderly sections.
    Signed-off-by: Marc Perrien <marc@chaosiq.io>

--- template-front.php
+++ template-front.php
@@ -51,6 +51,66 @@ get_header();
             </div>
           </section>
         <?php endif;
+        $adults = get_field('adults');
+        if ($adults) : ?>
+          <section class="audience audience--adults">
+            <div class="audience__content">
+              <h2 class="audience__title">
+                <?php echo esc_html($adults['title']); ?>
+              </h2>
+              <div class="audience__text">
+              <?php echo $adults['content']; ?>
+              </div>
+              <a href="<?php echo esc_url($adults['link']); ?>" class="button button--purple">
+                <?php echo esc_html($adults['button']); ?>
+              </a>
+            </div>
+            <div class="audience__image">
+            <?php
+              $image = $adults['image'];
+              $small = $image['sizes']['medium'];
+              $small_width = $image['sizes']['medium-width'];
+              $large = $image['sizes']['large'];
+              $large_width = $image['sizes']['large-width'];
+            ?>
+            <img srcset="<?php echo $small; ?> <?php echo $small_width; ?>w,
+                         <?php echo $large; ?> <?php echo $large_width; ?>w"
+                  sizes="(max-width: 480px) 480px,
+                          800px"
+                  src="<?php echo $large; ?>" alt="" loading="lazy">
+            </div>
+          </section>
+        <?php endif;
+        $elderly = get_field('elderly');
+        if ($elderly) : ?>
+          <section class="audience audience--elderly">
+            <div class="audience__content">
+              <h2 class="audience__title">
+                <?php echo esc_html($elderly['title']); ?>
+              </h2>
+              <div class="audience__text">
+              <?php echo $elderly['content']; ?>
+              </div>
+              <a href="<?php echo esc_url($elderly['link']); ?>" class="button button--blue">
+                <?php echo esc_html($elderly['button']); ?>
+              </a>
+            </div>
+            <div class="audience__image">
+            <?php
+              $image = $elderly['image'];
+              $small = $image['sizes']['medium'];
+              $small_width = $image['sizes']['medium-width'];
+              $large = $image['sizes']['large'];
+              $large_width = $image['sizes']['large-width'];
+            ?>
+            <img srcset="<?php echo $small; ?> <?php echo $small_width; ?>w,
+                         <?php echo $large; ?> <?php echo $large_width; ?>w"
+                  sizes="(max-width: 480px) 480px,
+                          800px"
+                  src="<?php echo $large; ?>" alt="" loading="lazy">
+            </div>
+          </section>
+        <?php endif;
       ?>
       </div>
 
