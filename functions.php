2b120576eaa99540e96fa7adb04678b7c569182c
Marc Perrien <marc@chaosiq.io>
2020-08-21 02:59:57 +0200

    Add support for page color themes.
    Signed-off-by: Marc Perrien <marc@chaosiq.io>

--- functions.php
+++ functions.php
@@ -181,6 +181,7 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+// Add an option page.
 if( function_exists('acf_add_options_page') ) {
 	acf_add_options_page();	
 }
